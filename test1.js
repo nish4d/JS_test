9f509dba309923a9258ed1d37e3142400d7a9a64
nishad <nishadisl4m@gmail.com>
2023-01-26 00:35:43 +0600

    first commit

---
+++ test1.js
@@ -0,0 +1,35 @@
+// test 1
+
+var orange = 1000;
+var apples = 700;
+console.log("orange = 1000 tk - apples = 700 tk");
+console.log("return = " + (orange - apples));
+
+// test 2
+
+var mathematics = 75.25;
+var biology = 65;
+var chemistry = 80;
+var physics = 35.45;
+var banGla = 99.50;
+/* Adding all the subject and dividing by 5. */
+var allSubject = (mathematics+biology+chemistry+physics+banGla)/5;
+var floatFix = allSubject.toFixed(2);
+console.log("2 decimal Place = "+ floatFix);
+
+// test 3
+
+var variable1 = "I am going to be";
+var variable2 = "an awesome web developer";
+console.log("One line = "+ (variable1+variable2));
+
+// test 4
+
+var number1 = 119;
+var number2 = 5;
+console.log("reminder = "+ (119%5))
+
+
+
+
+
