a3c0affa0538945b3b9041fe4575adcd819a7c2d
nishad <nishadisl4m@gmail.com>
2023-02-21 01:53:09 +0600

    added test 7

---
+++ test7.js
@@ -0,0 +1,14 @@
+// problem 1
+const threeParameters = (a, b, c) => a*b*c; 
+// console.log(threeParameters(2, 3, 4));
+
+const threeAddition = `I am a web developer. 
+I love to code. 
+I love to eat biryani.`
+// console.log(threeAddition);
+
+const aRro = (p1,p2=0) => p1+p2;
+const bRro = aRro(2);
+// console.log(bRro);
+
+// problem 2
