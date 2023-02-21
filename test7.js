c9872105b9e9c1b6457e0b1b27194c48deab60fd
nishad <nishadisl4m@gmail.com>
2023-02-21 09:47:55 +0600

    test 7 done

--- test7.js
+++ test7.js
@@ -12,3 +12,43 @@ const bRro = aRro(2);
 // console.log(bRro);
 
 // problem 2
+
+const filterEvenLengthNames = (friends) => {
+    const evenLengthNames = friends.filter((name) => name.length % 2 === 0);
+    return evenLengthNames;
+  };
+  const friendNames = ["Alice", "Bobs", "Charlie", "David", "Emily"];
+
+  const evenLengthNames = filterEvenLengthNames(friendNames);
+  
+//   console.log(evenLengthNames);
+
+
+//   problem 3
+const averageOfSquaredElements = (numbers) => {
+    const squaredNumbers = numbers.map((number) => Math.pow(number, 2));
+    const sumOfSquaredNumbers = squaredNumbers.reduce((acc, val) => acc + val, 0);
+    const average = sumOfSquaredNumbers / squaredNumbers.length;
+    return average;
+  };
+  const numbers = [2, 4, 6, 8];
+
+  const result = averageOfSquaredElements(numbers);
+  
+//   console.log(result); 
+
+//   problem 4
+const findMaxNumber = (array1, array2) => {
+    const combinedArray = [...array1, ...array2];
+    const maxNumber = Math.max(...combinedArray);
+    return maxNumber;
+  };
+const array1 = [1, 3, 5, 7];
+const array2 = [2, 4, 6, 8];
+
+const results = findMaxNumber(array1, array2);
+
+console.log(results);
+
+    
+    
